5dd28c24c18dbdba4068041b1fe9505e8b5d3607
Pawacode Database <whakhydoh@gmail.com>
2025-06-05 18:10:13 +0100

    lk

--- src/components/homeComponents/footer.tsx
+++ src/components/homeComponents/footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaTwitter, FaEnvelope } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
--- src/components/movingText.tsx
+++ src/components/movingText.tsx
@@ -4,7 +4,7 @@ const MovingText: React.FC = () => {
   return (
     <div className="overflow-hidden w-full bg-[#000] p-2">
       <div className="whitespace-nowrap animate-marquee text-sm  text-center text-[#ffffff]">
-       Follow us on<span className='text-[#01aefb] font-semibold'> on Tiktok </span>- <span className='text-[#01aefb] font-semibold'>@Babsteve</span>
+       Follow us <span className='text-[#01aefb] font-semibold'> on Facebook </span>- <span className='text-[#01aefb] font-semibold'>Babasteve Merchandise Enterprises</span>
       </div>
     </div>
   );
